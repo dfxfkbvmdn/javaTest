fa91c03b043d8c8f64cce930c8877ee4d2d4db31
정시윤 <YONSAI@402-18>
2021-07-08 09:04:04 +0900

    "Bravo Bravo"

---
+++ javaTest/src/kr/co/job/array/ArrayTest04.java
@@ -0,0 +1,37 @@
+/*
+ * 10개 배열을 생성하여 배열의값을 0~9까지 순차적으로 초기화하고
+ * 해당 배열값을 섞어보기
+ */
+package kr.co.job.array;
+
+import java.util.Arrays;
+
+public class ArrayTest04 {
+
+	public static void main(String[] args) {
+		// 
+		int[] numArr = new int[10];
+		int n=0, temp=0;
+		
+		for(int i=0;i<numArr.length;i++) {
+			numArr[i] = i; //배열을 0~9 숫자로 초기화
+			System.out.print(numArr[i]);
+		}
+		
+		for(int i=1;i<100;i++) {// 100회
+			n = (int)(Math.random()*10);
+			
+			temp = numArr[0];
+			numArr[0] = numArr[n];
+			numArr[n] = temp;
+		}
+		System.out.println(Arrays.toString(numArr));
+
+	}
+
+}
+
+
+
+
+
--- javaTest/src/kr/co/job/array/ArrayYest01.java
+++ javaTest/src/kr/co/job/array/ArrayYest01.java
@@ -5,26 +5,29 @@ import java.util.Arrays;
 public class ArrayYest01 {
 
 	public static void main(String[] args) {
-		//배열 or 리스트: 같은 타입의 변수를 하나의 묶음으로 다루는 것
-//		int[] score; 
+		//배열 or 리스트: 같은 타입의 변수를 하나의 묶음으로 다루는것 
+//		int[] score;
 //		score = new int[5];
 		int[] score = new int[5];
+		//초기화  init()
+		for(int i=0;i<score.length;i++) {
+			score[i] = 0;
+		}
 		
-		//초기화 init()
 //		score[0] = 55;//~ score[4] = 11
-		//배열에 랜덤으로 1~100까지의 수를 대입하기.
+		//배열에 랜덤으로 1~100까지 수를 대입하기.
 		//입력
 		for(int i=0;i<score.length;i++) {
-			score[i]=(int)(Math.random()*100+1);
+			score[i] = (int)(Math.random()*100)+1;
 		}
 		
-		//출력
+		//출력화면예시  score[0] = ?? 
 		for(int i=0;i<score.length;i++) {
-		System.out.print("score["+i+"]="+score[i]+"\n");
+			System.out.println("score["+i+"] = " + score[i]);
 		}
-		
 		//단순 배열의 값을 출력
-		System.out.println(Arrays.toString(score));
+		System.out.println(Arrays.toString(score));	
+		
 	}
 
 }
